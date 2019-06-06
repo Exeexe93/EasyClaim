1cda7025217f53083b6ff818f905a3186cbddc40
Exeexe <engxuanen@gmail.com>
2019-06-06 17:14:26 +0800

    Adding styles for the drawer items and logout in drawer and enable the logout text to go to login page when pressed

---
+++ android/app/src/main/component/Menu.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+import Styles from '../style/MenuStyle';
+
+export default class Menu extends Component {
+
+    render() {
+
+        const Logout = StackActions.reset({
+          index: 0,
+          key: null,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Login' }),
+          ],
+        });
+
+        return (
+            <View style = {Styles.container}>
+                <View style = { Styles.itemContainer }>
+                    <Text
+                      onPress = {() => this.props.navigation.navigate('Home')}
+                      style = { Styles.drawerItem }
+                      >
+                      Main
+                    </Text>
+                </View>
+                <View style = { Styles.logoutContainer}>
+                    <Text
+                      onPress = { () => this.props.navigation.dispatch(Logout) }
+                      style = { Styles.logout }
+                      >
+                      Logout
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
